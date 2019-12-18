7f503e0c4695f1397a24ffcef2f5933969a88fea
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-18 17:42:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Good", "Gooder", "Goodest", "Gooderest"]
+  ["Good", "Gooder", "Goodest", "Gooderest"]
 end
 
 def add_element_to_end_of_array(array, element)
